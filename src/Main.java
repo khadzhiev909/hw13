76f022b84c2fe17cdd5dba0e3c82b22e106d886b
Эпенди <khadzhiev909@gmail.com>
2023-03-19 17:18:26 +0300

    Готово.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+
+        //создайте несколько объектов «Книга» (достаточно двух) и
+        // несколько объектов «Автор» (достаточно тоже двух) и инициализируйте их.
+
+        //author
+        Author author = new Author("Лев", "Толстой");
+        Author author2 = new Author("Гузель", "Яхина");
+
+        //book
+
+        Book book2 = new Book("Гордость и предубеждения", author, 1222);
+        Book book1 = new Book("Living Time", author2,1890);
+
+
+
+        //В том же методе main измените год публикации одной из книг с помощью сеттера.
+
+        book1.setYear(1120);
+        System.out.println(book1.toString());
+        System.out.println("book1.year = " + book1.getYear());
+        System.out.println("book2.author = " + book2.getAuthor());
     }
 }
